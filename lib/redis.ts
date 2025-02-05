8de5665f79e934a18d38e6ceed9d45f8f977caf0
hespinoza <hespinoza@eikon.com.ec>
2025-02-04 21:01:20 -0500

    Simplificar la función getRedisUrl para retornar la URL de Redis directamente y eliminar la verificación de existencia de la variable de entorno

--- lib/redis.ts
+++ lib/redis.ts
@@ -4,11 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const getRedisUrl = () => {
-    if (process.env.REDIS_URL) {
-        return process.env.REDIS_URL || "redis://localhost:6379"
-    }
-
-    throw new Error("REDIS_URL is not defined");
+    return process.env.REDIS_URL || "redis://localhost:6379"
 }
 
 export const redis = new Redis(getRedisUrl());
